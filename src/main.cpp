f4f7dc31a42cf9d94d958114d5325ea881f82ec5
Morten Kristensen <msk@nullpointer.dk>
2014-06-04 20:20:31 +0200

    Temporary code for checking whether input file is MachO or not.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
+#include <QDebug>
+
+#include "formats/MachO.h"
+
 int main(int argc, char **argv) {
+  // Temporary!
+  if (argc != 2) {
+    qDebug() << "No file given";
+    return 0;
+  }
+
+  QString file{argv[1]};
+  qDebug() << "File:" << file;
+
+  MachO fmt(file);
+  qDebug() << "MachO:" << fmt.detect();
   return 0;
 }
