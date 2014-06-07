69d62381ad1198753e025c239bb82e8c6b08807b
Morten Kristensen <msk@nullpointer.dk>
2014-06-08 00:18:23 +0200

    Cleaned up ArchPane.

--- src/panes/ArchPane.cpp
+++ src/panes/ArchPane.cpp
@@ -17,31 +17,29 @@ void ArchPane::createLayout() {
   gridLayout->addWidget(new QLabel(Util::formatTypeString(obj->getFormatType())),
                         0, 1);
 
-                        /*
-  gridLayout->addWidget(new QLabel(tr("System bits:")), 0, 0);
+  gridLayout->addWidget(new QLabel(tr("System bits:")), 1, 0);
   gridLayout->addWidget(new QLabel(QString::number(obj->getSystemBits())),
-                        0, 1);
-                        */
+                        1, 1);
 
-  gridLayout->addWidget(new QLabel(tr("Little endian:")), 1, 0);
+  gridLayout->addWidget(new QLabel(tr("Little endian:")), 2, 0);
   gridLayout->addWidget(new QLabel(obj->isLittleEndian() ?
-                                   tr("Yes") : tr("No")), 1, 1);
+                                   tr("Yes") : tr("No")), 2, 1);
 
-  gridLayout->addWidget(new QLabel(tr("CPU type:")), 2, 0);
+  gridLayout->addWidget(new QLabel(tr("CPU type:")), 3, 0);
   gridLayout->addWidget(new QLabel(Util::cpuTypeString(obj->getCpuType())),
-                        2, 1);
-
-  gridLayout->addWidget(new QLabel(tr("CPU sub type:")), 3, 0);
-  gridLayout->addWidget(new QLabel(Util::cpuTypeString(obj->getCpuSubType())),
                         3, 1);
 
-  gridLayout->addWidget(new QLabel(tr("File type:")), 4, 0);
-  gridLayout->addWidget(new QLabel(Util::fileTypeString(obj->getFileType())),
+  gridLayout->addWidget(new QLabel(tr("CPU sub type:")), 4, 0);
+  gridLayout->addWidget(new QLabel(Util::cpuTypeString(obj->getCpuSubType())),
                         4, 1);
 
-  gridLayout->addWidget(new QLabel(tr("Sections:")), 5, 0);
-  gridLayout->addWidget(new QLabel(QString::number(obj->getSections().size())),
+  gridLayout->addWidget(new QLabel(tr("File type:")), 5, 0);
+  gridLayout->addWidget(new QLabel(Util::fileTypeString(obj->getFileType())),
                         5, 1);
+
+  gridLayout->addWidget(new QLabel(tr("Sections:")), 6, 0);
+  gridLayout->addWidget(new QLabel(QString::number(obj->getSections().size())),
+                        6, 1);
 
   auto *w = new QWidget;
   w->setMaximumWidth(300);
