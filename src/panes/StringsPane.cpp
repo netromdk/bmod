d4b87f8dc041da8fd0ad7dd997c1dbcf963937c7
Morten Kristensen <msk@nullpointer.dk>
2014-06-07 20:18:28 +0200

    Show strings on a list form beneath the raw form for explicitness.

--- src/panes/StringsPane.cpp
+++ src/panes/StringsPane.cpp
@@ -1,4 +1,7 @@
+#include <QDebug>
+#include <QSplitter>
 #include <QVBoxLayout>
+#include <QTreeWidget>
 
 #include "StringsPane.h"
 #include "../MachineCodeWidget.h"
@@ -9,12 +12,84 @@ StringsPane::StringsPane(BinaryObjectPtr obj)
   createLayout();
 }
 
+void StringsPane::showEvent(QShowEvent *event) {
+  QWidget::showEvent(event);
+  if (!shown) {
+    shown = true;
+    setup();
+  }
+}
+
 void StringsPane::createLayout() {
   auto *codeWidget = new MachineCodeWidget(obj, SectionType::CString);
 
+  treeWidget = new QTreeWidget;
+  treeWidget->setHeaderLabels(QStringList{tr("Address"), tr("String"),
+        tr("Length"), tr("Data")});
+  treeWidget->setColumnWidth(0, obj->getSystemBits() == 64 ? 110 : 70);
+  treeWidget->setColumnWidth(1, 200);
+  treeWidget->setColumnWidth(2, 50);
+  treeWidget->setColumnWidth(3, 200);
+
+  // Set fixed-width font.
+  treeWidget->setFont(QFont("Courier"));
+
+  auto *splitter = new QSplitter(Qt::Vertical);
+  splitter->addWidget(codeWidget);
+  splitter->addWidget(treeWidget);
+
   auto *layout = new QVBoxLayout;
   layout->setContentsMargins(0, 0, 0, 0);
-  layout->addWidget(codeWidget);
+  layout->addWidget(splitter);
   
   setLayout(layout);
+}
+
+void StringsPane::setup() {
+  SectionPtr sec = obj->getSection(SectionType::CString);
+  if (sec == nullptr) {
+    return;
+  }
+
+  quint64 addr = sec->getAddress();
+  const QByteArray &data = sec->getData();
+  if (data.isEmpty()) {
+    return;
+  }
+
+  QByteArray cur;
+  int len = data.size();
+  for (int i = 0; i < len; i++) {
+    char c = data[i];
+    cur += c;
+    if (c == 0) {
+      auto *item = new QTreeWidgetItem;
+
+      QString addrStr{QString::number(addr, 16).toUpper()};
+      int pad = obj->getSystemBits() / 8, addrLen = addrStr.size();
+      if (addrLen < pad) {
+        addrStr = QString(pad - addrLen, (char) 48) + addrStr;
+      }
+      item->setText(0, addrStr);
+
+      QString str = QString::fromUtf8(cur);
+      item->setText(1, str);
+      item->setText(2, QString::number(str.size()));
+
+      QString dataStr;
+      for (int j = 0; j < cur.size(); j++) {
+        QString hex{QString::number((unsigned char) cur[j], 16)};
+        if (hex.size() == 1) {
+          hex = "0" + hex;
+        }
+        dataStr += hex;
+      }
+      item->setText(3, dataStr.toUpper());
+
+      treeWidget->addTopLevelItem(item);
+      addr += cur.size();
+      cur.clear();
+      continue;
+    }
+  }
 }
--- src/panes/StringsPane.h
+++ src/panes/StringsPane.h
@@ -4,14 +4,23 @@
 #include "Pane.h"
 #include "../BinaryObject.h"
 
+class QTreeWidget;
+
 class StringsPane : public Pane {
 public:
   StringsPane(BinaryObjectPtr obj);
 
+protected:
+  void showEvent(QShowEvent *event);
+
 private:
   void createLayout();
+  void setup();
 
   BinaryObjectPtr obj;
+
+  bool shown;
+  QTreeWidget *treeWidget;
 };
 
 #endif // BMOD_STRINGS_PANE_H
