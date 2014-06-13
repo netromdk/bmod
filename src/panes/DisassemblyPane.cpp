851dad66b202444236d5fc665678545231df8505
Morten Kristensen <msk@nullpointer.dk>
2014-06-13 02:20:27 +0200

    Implemented support for editing machine code data in the DisassemblyPane.

--- src/panes/DisassemblyPane.cpp
+++ src/panes/DisassemblyPane.cpp
@@ -1,12 +1,83 @@
+#include <QDebug>
 #include <QLabel>
+#include <QLineEdit>
 #include <QVBoxLayout>
 #include <QApplication>
 #include <QProgressDialog>
+#include <QStyledItemDelegate>
 
 #include "../Util.h"
 #include "DisassemblyPane.h"
 #include "../asm/Disassembler.h"
 #include "../widgets/TreeWidget.h"
+
+namespace {
+  class ItemDelegate : public QStyledItemDelegate {
+  public:
+    ItemDelegate(QTreeWidget *tree, SectionPtr sec) : tree{tree}, sec{sec} { }
+
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
+                          const QModelIndex &index) const {
+      int col = index.column();
+      if (col != 1) {
+        return nullptr;
+      }
+
+      QString text = index.data().toString().trimmed();
+      qDebug() << text;
+      if (text.isEmpty()) {
+        return nullptr;
+      }
+
+      QString mask;
+      int blocks = text.split(" ").size();
+      for (int i = 0; i < blocks; i++) {
+        mask += "HH ";
+      }
+      if (mask.endsWith(" ")) {
+        mask.chop(1);
+      }
+
+      auto *edit = new QLineEdit(parent);
+      edit->setInputMask(mask);
+      edit->setText(text);
+      return edit;
+    }
+
+    void setModelData(QWidget *editor, QAbstractItemModel *model,
+                      const QModelIndex &index) const {
+      auto *edit = qobject_cast<QLineEdit*>(editor);
+      if (edit) {
+        QString oldStr = model->data(index).toString().trimmed();
+        QString newStr = edit->text().toUpper();
+        if (newStr == oldStr) {
+          return;
+        }
+        model->setData(index, newStr);
+        auto *item = tree->topLevelItem(index.row());
+        if (item) {
+          int col = index.column();
+          auto font = item->font(col);
+          font.setBold(true);
+          item->setFont(col, font);
+          item->setForeground(col, Qt::red);
+
+          // Change region.
+          quint64 addr = item->text(0).toULongLong(nullptr, 16);
+          quint64 pos = addr - sec->getAddress();
+          QByteArray data = Util::hexToData(newStr.replace(" ", ""));
+          sec->setSubData(data, pos);
+
+          // TODO: update the disassembly
+        }
+      }
+    }
+
+  private:
+    QTreeWidget *tree;
+    SectionPtr sec;
+  };
+}
 
 DisassemblyPane::DisassemblyPane(BinaryObjectPtr obj, SectionPtr sec)
   : Pane(Kind::Disassembly), obj{obj}, sec{sec}, shown{false}
@@ -37,6 +108,7 @@ void DisassemblyPane::createLayout() {
   treeWidget->setColumnWidth(0, obj->getSystemBits() == 64 ? 110 : 70);
   treeWidget->setColumnWidth(1, 200);
   treeWidget->setColumnWidth(2, 200);
+  treeWidget->setItemDelegate(new ItemDelegate(treeWidget, sec));
 
   auto *layout = new QVBoxLayout;
   layout->setContentsMargins(0, 0, 0, 0);
@@ -70,6 +142,7 @@ void DisassemblyPane::setup() {
       short bytes = result.bytesConsumed[i];
 
       auto *item = new QTreeWidgetItem;
+      item->setFlags(Qt::ItemIsEditable | Qt::ItemIsEnabled | Qt::ItemIsSelectable);
       item->setText(0, Util::padString(QString::number(addr, 16).toUpper(),
                                        obj->getSystemBits() / 8));
 
