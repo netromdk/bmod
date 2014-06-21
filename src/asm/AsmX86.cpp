79ab149d8c535293b6a3a3b0c227daabc6113407
Morten Kristensen <msk@nullpointer.dk>
2014-06-22 01:10:35 +0200

    Restarted X86 disassembling.

--- src/asm/AsmX86.cpp
+++ src/asm/AsmX86.cpp
@@ -12,6 +12,12 @@
 #include "../Reader.h"
 #include "../Section.h"
 
+namespace {
+  QString Instruction::toString() const {
+    return "not implemented yet";
+  }
+}
+
 AsmX86::AsmX86(BinaryObjectPtr obj) : obj{obj}, reader{nullptr} { }
 
 bool AsmX86::disassemble(SectionPtr sec, Disassembly &result) {
@@ -28,307 +34,19 @@ bool AsmX86::disassemble(SectionPtr sec, Disassembly &result) {
   quint32 num{0};
   qint64 pos{0};
   while (!reader->atEnd()) {
-    // Handle special NOP sequences.
-    if (handleNops(result)) {
-      continue;
-    }
-
-    // Handle other instructions.
     pos = reader->pos();
     ch = reader->getUChar(&ok);
     if (!ok) return false;
 
     nch = reader->peekUChar(&peek);
 
-    // ADD (r/m16/32  r16/32)
-    if (ch == 0x01 && peek) {
-      reader->getUChar(); // eat
-      addResult("addl " + getModRMByte(nch, RegType::R32, RegType::R32),
-                pos, result);
-    }
-
-    // ADD (r16/32  r/m16/32)
-    else if (ch == 0x03 && peek) {
-      reader->getUChar(); // eat
-      addResult("addl " + getModRMByte(nch, RegType::R32, RegType::R32),
-                pos, result);
-    }
-
-    // AND (eAX  imm16/32)
-    else if (ch == 0x25) {
-      num = reader->getUInt32(&ok);
-      if (!ok) return false;
-      addResult("addl $" + formatHex(num, 8) + ",%eax", pos, result);
-    }
-
-    // CMP (r16/32 r/m16/32) (reverse)
-    else if (ch == 0x3B && peek) {
-      reader->getUChar(); // eat
-      addResult("cmpl " + getModRMByte(nch, RegType::R32, RegType::R32, true),
-                pos, result);
-    }
-
-    // CMP (eAX  imm16/32)
-    else if (ch == 0x3D) {
-      num = reader->getUInt32(&ok);
-      if (!ok) return false;
-      addResult("cmpl $" + formatHex(num, 8) + ",%eax", pos, result);
-    }
-
     // PUSH
-    else if (ch >= 0x50 && ch <= 0x57) {
-      r = ch - 0x50;
-      addResult("pushl " + getReg(RegType::R32, r), pos, result);
+    if (ch >= 0x50 && ch <= 0x57) {
+      // 1. set instruction type (PUSH)
+      // 2. find r value and set register
+      // 3. convert to string value and add result
     }
 
-    // POP
-    else if (ch >= 0x58 && ch <= 0x60) {
-      r = ch - 0x58;
-      addResult("popl " + getReg(RegType::R32, r), pos, result);
-    }
-
-    // ADD, OR, ADC, SBB, AND, SUB, XOR, CMP
-    // (r/m16/32	imm16/32)
-    else if (ch == 0x81 && peek) {
-      reader->getUChar(); // eat
-      splitByteModRM(nch, mod, op1, op2);
-
-      num = reader->getUInt32(&ok);
-      if (!ok) return false;
-
-      QString inst;
-      if (op1 == 0) {
-        inst = "addl";
-      }
-      else if (op1 == 1) {
-        inst = "orl";
-      }
-      else if (op1 == 2) {
-        inst = "adcl"; // Add with carry
-      }
-      else if (op1 == 3) {
-        inst = "sbbl"; // Integer subtraction with borrow
-      }
-      else if (op1 == 4) {
-        inst = "andl";
-      }
-      else if (op1 == 5) {
-        inst = "subl";
-      }
-      else if (op1 == 6) {
-        inst = "xorl";
-      }
-      else if (op1 == 7) {
-        inst = "cmpl";
-      }
-
-      addResult(inst + " $" + formatHex(num, 8) + "," +
-                getReg(RegType::R32, op2), pos, result);
-    }
-
-    // ADD, OR, ADC, SBB, AND, SUB, XOR, CMP
-    // (r/m16/32	imm8)
-    else if (ch == 0x83 && peek) {
-      reader->getUChar(); // eat
-      splitByteModRM(nch, mod, op1, op2);
-
-      ch2 = reader->getUChar(&ok);
-      if (!ok) return false;
-
-      QString inst;
-      if (op1 == 0) {
-        inst = "addl";
-      }
-      else if (op1 == 1) {
-        inst = "orl";
-      }
-      else if (op1 == 2) {
-        inst = "adcl"; // Add with carry
-      }
-      else if (op1 == 3) {
-        inst = "sbbl"; // Integer subtraction with borrow
-      }
-      else if (op1 == 4) {
-        inst = "andl";
-      }
-      else if (op1 == 5) {
-        inst = "subl";
-      }
-      else if (op1 == 6) {
-        inst = "xorl";
-      }
-      else if (op1 == 7) {
-        inst = "cmpl";
-      }
-
-      addResult(inst + " $" + formatHex(ch2, 2) + "," +
-                getReg(RegType::R32, op2), pos, result);
-    }
-
-    // MOV (r/m8	r8)
-    else if (ch == 0x88 && peek) {
-      reader->getUChar(); // eat
-      addResult("movb " + getModRMByte(nch, RegType::R8, RegType::R32),
-                pos, result);
-    }
-
-    // MOV (r/m16/32	r16/32)
-    else if (ch == 0x89 && peek) {
-      reader->getUChar(); // eat
-      addResult("movl " + getModRMByte(nch, RegType::R32, RegType::R32),
-                pos, result);
-    }
-
-    // MOV (r8  r/m8) (reverse of 0x88)
-    else if (ch == 0x8A && peek) {
-      reader->getUChar(); // eat
-      addResult("movb " + getModRMByte(nch, RegType::R8, RegType::R32, true),
-                pos, result);
-    }
-
-    // MOV (r16/32	r/m16/32) (reverse of 0x89)
-    else if (ch == 0x8B) {
-      reader->getUChar(); // eat
-      addResult("movl " + getModRMByte(nch, RegType::R32, RegType::R32, true),
-                pos, result);
-    }
-
-    // LEA (r16/32	m) Load Effective Address
-    else if (ch == 0x8D && peek) {
-      reader->getUChar(); // eat
-      if (!ok) return false;
-      addResult("leal " + getModRMByte(nch, RegType::R32, RegType::R32, true),
-                pos, result);
-    }
-
-    // NOP
-    else if (ch == 0x90) {
-      addResult("nop", pos, result);
-    }
-
-    // TEST	(AL	imm8)
-    else if (ch == 0xA8 && peek) {
-      reader->getUChar(); // eat
-      addResult("testb $" + formatHex(nch, 2) + ",%al", pos, result);
-    }
-
-    // MOV (r16/32	imm16/32)
-    else if (ch >= 0xB8 && ch <= 0xBF) {
-      splitByteModRM(ch, mod, op1, op2);
-      num = reader->getUInt32(&ok);
-      if (!ok) return false;
-      addResult("movl $" + formatHex(num, 8) + "," +
-                getModRMByte(op2, RegType::R32, RegType::R32), pos, result);
-    }
-
-    // RETN
-    else if (ch == 0xC3) {
-      addResult("ret", pos, result);
-    }
-
-    // MOV (r/m8  imm8)
-    else if (ch == 0xC6 && peek) {
-      reader->getUChar(); // eat
-      if (!ok) return false;
-      addResult("movb " + getModRMByte(nch, RegType::R8, RegType::R32, false, 0),
-                pos, result);
-    }
-
-    // MOV (r/m16/32	imm16/32)
-    else if (ch == 0xC7 && peek) {
-      reader->getUChar(); // eat
-      splitByteModRM(nch, mod, op1, op2);
-      ch2 = reader->getUChar(&ok);
-      if (!ok) return false;
-      num = reader->getUInt32(&ok);
-      if (!ok) return false;
-      addResult("movl $" + formatHex(num, 8) + "," + formatHex(ch2, 2) + "(" +
-                getReg(RegType::R32, op2) + ")", pos, result);
-    }
-
-    // Call (relative function address)
-    else if (ch == 0xE8) {
-      num = reader->getUInt32(&ok);
-      if (!ok) return false;
-      addResult("calll " + formatHex(funcAddr + reader->pos() + num, 8),
-                pos, result);
-    }
-
-    // JMP (rel16/32) (relative address)
-    else if (ch == 0xE9) {
-      num = reader->getUInt32(&ok);
-      if (!ok) return false;
-      addResult("jmp " + formatHex(funcAddr + reader->pos() + num, 8),
-                pos, result);
-    }
-
-    // INC, DEC, CALL, CALLF, JMP, JMPF, PUSH
-    else if (ch == 0xFF && peek) {
-      reader->getUChar(); // eat
-      splitByteModRM(nch, mod, op1, op2);
-
-      QString inst;
-      if (op1 == 0) {
-        inst = "inc ";
-      }
-      else if (op1 == 1) {
-        inst = "dec ";
-      }
-      else if (op1 == 2) {
-        inst = "call *";
-      }
-      else if (op1 == 3) {
-        inst = "callf ";
-      }
-      else if (op1 == 4) {
-        inst = "jmp ";
-      }
-      else if (op1 == 5) {
-        inst = "jmpf ";
-      }
-      else if (op1 == 6) {
-        inst = "pushl ";
-      }
-
-      addResult(inst + getReg(RegType::R32, op2), pos, result);
-    }
-
-    // Two-byte instructions.
-    else if (ch == 0x0F && peek) {
-      ch = nch;
-      reader->getUChar(); // eat
-
-      nch = reader->peekUChar(&peek);
-
-      // NOP (r/m16/32)
-      if (ch == 0x1F && peek) {
-        reader->getUChar(); // eat
-        splitByteModRM(nch, mod, op1, op2);
-        num = reader->getUInt32(&ok);
-        if (!ok) return false;
-        addResult("nopl " + formatHex(num, 8) +
-                  "(" + getReg(RegType::R32, op1) + ")",
-                  pos, result);
-      }
-
-      // JNZ (rel16/32) or JNE (rel16/32), same functionality
-      // different name. Relative function address.
-      else if (ch == 0x85) {
-        num = reader->getUInt32(&ok);
-        if (!ok) return false;
-        addResult("jne " + formatHex(funcAddr + reader->pos() + num, 8),
-                  pos, result);
-      }
-
-      // MOVSX (r16/32 r/m8) (reverse)
-      // Move with Sign-Extension
-      else if (ch == 0xBE && peek) {
-        reader->getUChar(); // eat
-        if (!ok) return false;
-        addResult("movsbl " + getModRMByte(nch, RegType::R32, RegType::R8, true),
-                  pos, result);
-      }
-    }
 
     // Unsupported
     else {
@@ -340,63 +58,19 @@ bool AsmX86::disassemble(SectionPtr sec, Disassembly &result) {
   return !result.asmLines.isEmpty();
 }
 
-bool AsmX86::handleNops(Disassembly &result) {
-  qint64 pos = reader->pos();
-  if (reader->peekList({0x66, 0x90})) {
-    reader->read(2);
-    addResult("xchg %ax,%ax", pos, result);
-    return true;
-  }
-  else if (reader->peekList({0x0f, 0x1f, 0x00})) {
-    reader->read(3);
-    addResult("nopl 0x0(%eax)", pos, result);
-    return true;
-  }
-  else if (reader->peekList({0x0f, 0x1f, 0x44, 0x00, 0x00})) {
-    reader->read(5);
-    addResult("nopl 0x0(%eax,%eax)", pos, result);
-    return true;
-  }
-  else if (reader->peekList({0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00})) {
-    reader->read(6);
-    addResult("nopw 0x0(%eax,%eax)", pos, result);
-    return true;
-  }
-  else if (reader->peekList({0x0f, 0x1f, 0x80, 0x00, 0x00, 0x00, 0x00})) {
-    reader->read(7);
-    addResult("nopl 0L(%eax)", pos, result);
-    return true;
-  }
-  else if (reader->peekList({0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00})) {
-    reader->read(8);
-    addResult("nopl 0L(%eax,%eax)", pos, result);
-    return true;
-  }
-  else if (reader->peekList({0x66, 0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00})) {
-    reader->read(9);
-    addResult("nopw 0L(%eax,%eax)", pos, result);
-    return true;
-  }
-  else if (reader->peekList({0x66, 0x2e, 0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00})) {
-    reader->read(10);
-    addResult("nopw %cs:0L(%eax,%eax)", pos, result);
-    return true;
-  }
-  return false;
-}
-
 void AsmX86::addResult(const QString &line, qint64 pos, Disassembly &result) {
   result.asmLines << line;
   result.bytesConsumed << reader->pos() - pos;
 }
 
-void AsmX86::splitByteModRM(unsigned char num, unsigned char &mod,
-                            unsigned char &op1, unsigned char &op2) {
+void AsmX86::splitByte(unsigned char num, unsigned char &mod, unsigned char &op1,
+                       unsigned char &op2) {
   mod = (num & 0xC0) >> 6; // 2 first bits
   op1 = (num & 0x38) >> 3; // 3 next bits
   op2 = num & 0x7; // 3 last bits  
 }
 
+/*
 QString AsmX86::getReg(RegType type, int num) {
   if (num < 0 || num > 7) {
     return QString();
@@ -407,72 +81,9 @@ QString AsmX86::getReg(RegType type, int num) {
                                {"mm0", "mm1", "mm2", "mm3", "mm4", "mm5", "mm6", "mm7"},
                                {"xmm0", "xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7"},
                                {"es", "cs", "ss", "ds", "fs", "gs", "reserved", "reserved"}};
-  return "%" + regs[type][num];
+  return "%" + regs[(int) type][num];
 }
-
-QString AsmX86::getModRMByte(unsigned char num, RegType type1, RegType type2,
-                             bool swap, int imm) {
-  unsigned char mod, op1, op2;
-  splitByteModRM(num, mod, op1, op2);
-
-  // [reg]
-  if (mod == 0) {
-    if (op1 != 4 && op1 != 5 && op2 != 4 && op2 != 5) {
-      return (!swap
-              ? getReg(type1, op1) + ",(" + getReg(type2, op2) + ")"
-              : "(" + getReg(type2, op2) + ")," + getReg(type1, op1));
-    }
-    else if (op1 == 4) {
-      unsigned char num = reader->getUChar();
-      unsigned char m, o1, o2;
-      splitByteModRM(num, m, o1, o2);
-      return getReg(type1, op2) + ",(" + getReg(type2, o2) + ")";
-    }
-    else if (op2 == 4) {
-      unsigned char num = reader->getUChar();
-      unsigned char m, o1, o2;
-      splitByteModRM(num, m, o1, o2);
-      return getReg(type1, op1) + ",(" + getReg(type2, o1) + ")";
-    }
-    else {
-      qDebug() << "unsupported mod=0 op1/op2 = 5" << op1 << op2;
-      qDebug() << "this: " << QString::number(num, 16);
-    }
-  }
-
-  // [reg]+disp8
-  else if (mod == 1) {
-    unsigned char num = reader->getUChar();
-    QString imms;
-    if (imm != -1) {
-      unsigned char num2 = reader->getUChar();
-      imms = "$" + formatHex(num2, 2);
-    }
-    return (!swap
-            ? (imm == 0 ? imms : getReg(type1, op1)) + "," +
-              formatHex(num, 2) + "(" + getReg(type2, op2) + ")"
-            : formatHex(num, 2) + "(" + getReg(type2, op2) + ")," +
-              (imm == 0 ? imms : getReg(type1, op1)));
-  }
-
-  // [reg]+disp32
-  else if (mod == 2) {
-    quint32 num = reader->getUInt32();
-    return (!swap
-            ? getReg(type1, op1) + "," + formatHex(num, 8) + "(" +
-            getReg(type2, op2) + ")"
-            : formatHex(num, 8) + "(" + getReg(type2, op2) + ")," +
-            getReg(type1, op1));
-  }
-
-  else if (mod == 3) {
-    return (!swap
-            ? getReg(type1, op1) + "," + getReg(type2, op2)
-            : getReg(type2, op2) + "," + getReg(type1, op1));
-  }
-
-  return QString();
-}
+*/
 
 QString AsmX86::formatHex(quint32 num, int len) {
   return "0x" + Util::padString(QString::number(num, 16), len);
--- src/asm/AsmX86.h
+++ src/asm/AsmX86.h
@@ -6,13 +6,49 @@
 #include "../BinaryObject.h"
 
 namespace {
-  enum RegType {
+  enum class RegType : int {
     R8,  // 8-bit register
     R16, // 16-bit register
     R32, // 32-bit register
     MM,  // MMX register
     XMM, // 128-bit register
     SREG // Segment register
+  };
+
+  enum class Register : int {
+    // 8-bit registers
+    AL, CL, DL, BL, AH, CH, DH, BH,
+
+    // 16-bit registers
+    AX, CX, DX, BX, SP, BP, SI, DI,
+
+    // 32-bit registers
+    EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI,
+
+    // MMX registers
+    MM0, MM1, MM2, MM3, MM4, MM5, MM6, MM7,
+
+    // 128-bit registers
+    XMM0, XMM1, XMM2, XMM3, XMM4, XMM5, XMM6, XMM7,
+
+    // Segment registers
+    ES, CS, SS, DS, FS, GS
+  };
+
+  class Instruction {
+  public:
+    Instruction()
+      : opcode{0}, mod{-1}, reg{-1}, rm{-1}, scale{-1}, index{-1}, base{-1},
+      disp{0}, imm{0}
+    { }
+
+    QString toString() const;
+
+    unsigned char opcode;
+    char mod, reg, rm; // mod-reg-r/m values
+    char scale, index, base; // SIP values
+    qint32 disp, // displacement
+      imm; // immediate data
   };
 }
 
@@ -22,26 +58,14 @@ public:
   bool disassemble(SectionPtr sec, Disassembly &result);
 
 private:
-  bool handleNops(Disassembly &result);
   void addResult(const QString &line, qint64 pos, Disassembly &result);
 
   // Split byte into [2][3][3] bits.
-  void splitByteModRM(unsigned char num, unsigned char &mod,
-                      unsigned char &op1, unsigned char &op2);
+  void splitByte(unsigned char num, unsigned char &mod, unsigned char &op1,
+                 unsigned char &op2);
 
   // 0 <= num <= 7
-  QString getReg(RegType type, int num);
-
-  /**
-   * num is either a general-purpose register (r) or a memory address
-   * (m). swap means to swap the order of the [op1] and [op2] part of
-   * num and any displacement.
-   *
-   * 'imm' is for using an intermediate value instead of a register,
-   * the value can be 0 or 1, but -1 means unused.
-   */
-  QString getModRMByte(unsigned char num, RegType type1, RegType type2,
-                       bool swap = false, int imm = -1);
+  //QString getReg(RegType type, int num);
 
   // Format and pad num as "0x.."
   QString formatHex(quint32 num, int len = 2);
